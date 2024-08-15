7faf7eb49d108c3a80422501b5aa91e5f6268f55
Tom Christensen <82264960+TomChristensen474@users.noreply.github.com>
2024-08-15 20:41:15 +0100

    handling network dropouts, but also breaking the existing code

--- src/pinger.py
+++ src/pinger.py
@@ -1,9 +1,15 @@
 from pythonping import ping
 from sqlite3 import Connection, Error
-from time import gmtime, strftime
+from time import localtime, strftime
 # from datetime import datetime
 
-from src import db_connector
+import db_connector
+
+def do_ping():
+    try:
+        return ping('8.8.8.8', interval=1, verbose=False) # one ping every second
+    except Exception as e:
+        return False
 
 def ping_and_record():
     connection = db_connector.create_connection("network_history.db")
@@ -12,19 +18,18 @@ def ping_and_record():
     if connection:
         cursor = connection.cursor()
 
-        ping_result = ping('8.8.8.8', interval=1, verbose=False) # one ping every second
-        datetime = strftime("%F %T", gmtime())
+        ping_result = do_ping()
+        datetime = strftime("%F %T", localtime())
         # datetime = datetime.now()
         print(datetime)
 
-         # option 3 requires success on MOST packets
-        if ping_result.success(option=2): # type: ignore
+        if ping_result:
             print("Average ping time (ms): " + str(ping_result.rtt_avg_ms))
             print("Packet loss: " + str(ping_result.packet_loss))
             data = (datetime, ping_result.rtt_avg_ms, ping_result.packet_loss, False)
         else:
             print("Network down")
-            data = (datetime, None, ping_result.packet_loss, True)
+            data = (datetime, None, 1, True)
 
         cursor.execute("INSERT INTO network_history VALUES(NULL, ?, ?, ?, ?)", data)
         connection.commit()
