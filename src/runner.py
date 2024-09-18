e60c6f4297e7af71b25dd020bee1e6746ea6d4cf
Tom Christensen <82264960+TomChristensen474@users.noreply.github.com>
2024-09-18 16:30:34 +0100

    adding new thread for server and updating default ping frequency

--- src/runner.py
+++ src/runner.py
@@ -1,15 +1,20 @@
 import schedule
 import time
+from threading import Thread
+
 import setup_database
+from server import start_server
 
 from bar_chart import chart
 from clear_old_data import remove_data_before_last_x_days
 from pinger import ping_and_record
 
-schedule.every(1).minutes.do(ping_and_record)
+schedule.every(30).seconds.do(ping_and_record)
 schedule.every(1).minutes.do(chart)
-schedule.every().day.at("00:00").do(remove_data_before_last_x_days, 3)
+schedule.every().day.at("00:00").do(remove_data_before_last_x_days, 2)
 
 while True:
+    thread = Thread(target=start_server)
+    thread.start() # start the server in a separate thread
     schedule.run_pending()
-    time.sleep(1)
+    time.sleep(5)
